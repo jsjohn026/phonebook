043c3b9fca4b5ead4fb14c2871c3f4d56f164ee8
Jasmine John <jsjohn026@gmail.com>
2025-05-19 11:43:22 -0500

    added phone number validation for length and format

--- models/person.js
+++ models/person.js
@@ -22,7 +22,13 @@ const personSchema = new mongoose.Schema({
   number: {
     type: String,
     minLength: 8,
-    required: true
+    validate: {
+      validator: function(val) {
+        return /^(\d{2}|\d{3})-\d+$/.test(val)
+      },
+      message: props => `${props.value} is not a valid phone number!`,
+    },
+    required: [true, "Phone number is required"]
   }
 })
 
